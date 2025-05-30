f81ce1748bcc59da30b108d0f09edf68454fe743
Elizabeth C. <yani95cisneros@gmail.com>
2025-05-30 17:21:39 -0300

    LastPush_v1

--- Assets/Scripts/Proyectiles/LanzamientoRoca.cs
+++ Assets/Scripts/Proyectiles/LanzamientoRoca.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OtterAttack : MonoBehaviour
+public class LanzamientoRoca : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
--- Assets/Scripts/Stats/OtterLife.cs
+++ Assets/Scripts/Stats/OtterLife.cs
@@ -2,8 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OtterAttack : MonoBehaviour
+public class OtterLife : MonoBehaviour
+
 {
+    private int Life = 100;
+    private int Damage = 10;
+    private bool OtterOn;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,6 @@ public class OtterAttack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
 }
